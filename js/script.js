a0845be43c48b73471bfabae461af35e3eb33973
Alberto G <aguer91@gmail.com>
2021-03-08 21:12:06 +0100

    agregados varios comentarios para probar un deshacer en GIT

--- js/script.js
+++ js/script.js
@@ -17,3 +17,5 @@ function quitarVida3(){
 corazon1.addEventListener("click", quitarVida1);
 corazon2.addEventListener("click", quitarVida2);
 corazon3.addEventListener("click", quitarVida3);
+
+//  Comentario
